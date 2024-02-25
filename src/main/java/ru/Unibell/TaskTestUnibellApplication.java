6f498b5445db1c02c7710101238a78ed107653ce
Viacheslav <119102560+Viacheslav666@users.noreply.github.com>
2024-02-25 19:29:12 +0400

    First

---
+++ src/main/java/ru/Unibell/TaskTestUnibellApplication.java
@@ -0,0 +1,13 @@
+package ru.Unibell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTestUnibellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTestUnibellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/Unibell/TastTestUnibellApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Unibell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastTestUnibellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
